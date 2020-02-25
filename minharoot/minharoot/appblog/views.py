ee56679afaa5959f69f8d7f760dcbb6b6f1ea74f
M0acyr <moacyr.soares@etec.sp.gov.br>
2020-02-24 21:07:29 -0300

    Atualização template para mostrar posts do banco de dados.

--- minharoot/minharoot/appblog/views.py
+++ minharoot/minharoot/appblog/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render
 
 # Create your views here.
 from django.http import request
+from .models import Post
+from django.utils import timezone
+
 
 def post_list(request):
-    return render(request, 'post_list.html', {})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'appblog/post_list.html', {'posts': posts})
