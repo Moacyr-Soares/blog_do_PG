428249451d66d73a0e3ff7e402351b992bc20eee
M0acyr <moacyr.soares@etec.sp.gov.br>
2020-02-27 18:39:16 -0300

    Adicão de view(post_list) e tamplate(post_list) para exibir conteúdo de post. Adição de css

--- minharoot/minharoot/appblog/views.py
+++ minharoot/minharoot/appblog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 # Create your views here.
 from django.http import request
@@ -9,3 +9,8 @@ from django.utils import timezone
 def post_list(request):
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
     return render(request, 'appblog/post_list.html', {'posts': posts})
+
+
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request,'appblog/post_detail.html', {'post': post})
